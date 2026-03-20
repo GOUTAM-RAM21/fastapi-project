e1f587a77f11572a7c0603a16203b77dfa479f7b
Goutam Das <goutamdas@gmail.com>
2026-03-20 22:08:03 +0530

    fix: add CORS middleware

--- app/main.py
+++ app/main.py
@@ -1,10 +1,19 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from prometheus_fastapi_instrumentator import Instrumentator
 from app.api import routes_auth, routes_predict
 from app.middleware.logging_middleware import LoggingMiddleware
 from app.core.exceptions import register_exception_handlers
 
 app = FastAPI(title='Car Price Prediction API')
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 # link middleware
 app.add_middleware(LoggingMiddleware)
